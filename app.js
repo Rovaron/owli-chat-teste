f8ffbac4f276c8e4b7baaa3e18d1df6af952acf3
Rovaron <gui.rovaron@gmail.com>
2020-02-27 15:22:19 -0300

    removing config

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-corsOptions = {
-  origin: "https://owli-chat-test.firebaseapp.com",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan('dev'))
 app.use(helmet())
 app.use('/api', require('./api'))
--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SECRET: process.env.NODE_ENV === 'production' ? process.env.SECRET : '*]hc2X8Gjn3/P.ZP)kMqrD_BAcRngL',
-  CONNECTION_STRING: "mongodb+srv://Shawii:uWbtLL3AHiptArRi@owli-chat-test-oeegm.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  SECRET: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'seu-segredo',
+  CONNECTION_STRING: "string mongo driver",
   SALT_ROUNDS: 10
 }
